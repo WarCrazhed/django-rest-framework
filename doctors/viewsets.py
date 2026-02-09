da558250edf4f462b301b76740a68f678017172a
Mario Zamora <zamora@Mac-mini-de-Mario-5.local>
2026-02-09 08:53:10 -0600

    decorator @action

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .serializers import DoctorSerializer
 from .models import Doctor
 
@@ -6,3 +8,16 @@ class DoctorViewSet(viewsets.ModelViewSet):
     serializer_class = DoctorSerializer
     queryset = Doctor.objects.all()
 
+    @action(['POST'], detail=True, url_path='set-on-vacation')
+    def set_on_vacation(self, request, pk):
+        doctor = self.get_object()
+        doctor.is_on_vacation = True
+        doctor.save()
+        return Response({'status': 'Doctor is on vacation'})
+        
+    @action(['POST'], detail=True, url_path='set-off-vacation')
+    def set_off_vacation(self, request, pk):
+        doctor = self.get_object()
+        doctor.is_on_vacation = False
+        doctor.save()
+        return Response({'status': 'Doctor is not on vacation'})
