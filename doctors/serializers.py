ae3bf365f23ef2c7e4089ca2254f5b0db6ebea24
Mario Zamora <zamora@Mac-mini-de-Mario-5.local>
2026-02-10 11:24:11 -0600

    custom validates, nested serializers and serializer_method_field

--- doctors/serializers.py
+++ doctors/serializers.py
@@ -6,6 +6,17 @@ class DoctorSerializer(serializers.ModelSerializer):
         model = Doctor
         fields = "__all__"
 
+    def validate_email(self, value):
+        # value = "hola@example.com"
+        if "@example.com" in value:
+            return value
+        raise serializers.ValidationError("Email must be from the domain example.com")
+    
+    def validate(self, attrs):
+        if len(attrs['contact_number']) < 10 and attrs['is_on_vacation'] == True:
+            raise serializers.ValidationError("Contact number must be 10 digits when doctor is on vacation")
+        return super().validate(attrs)
+
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
