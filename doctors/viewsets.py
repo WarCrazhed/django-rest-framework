54436f58b63f8f879a9fd7186946770dd3221ace
Mario Zamora <zamora@Mac-mini-de-Mario-5.local>
2026-02-11 09:13:28 -0600

    Nested endpoints doctor appointments

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -1,10 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .serializers import DoctorSerializer
 from .models import Doctor
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .permissions import IsDoctor
+from bookings.serializers import AppointmentSerializer
+from bookings.models import Appointment
 
 class DoctorViewSet(viewsets.ModelViewSet):
     serializer_class = DoctorSerializer
@@ -24,3 +26,22 @@ class DoctorViewSet(viewsets.ModelViewSet):
         doctor.is_on_vacation = False
         doctor.save()
         return Response({'status': 'Doctor is not on vacation'})
+
+    @action(['POST', 'GET'], detail=True, serializer_class=AppointmentSerializer)
+    def appointments(self, request, pk):
+        doctor = self.get_object()
+
+        if request.method == 'POST':
+            data = request.data.copy()
+            data['doctor'] = doctor.id
+            serializer = AppointmentSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        if request.method == 'GET':
+            appointments = doctor.appointments.all()
+            # Otra forma:
+            # appointments = Appointment.objects.filter(doctor=doctor)
+            serializer = AppointmentSerializer(appointments, many=True)
+            return Response(serializer.data)
