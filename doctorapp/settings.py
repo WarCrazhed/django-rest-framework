237962a163f22b23d75284bf798757d09b82586a
Mario Zamora <zamora@Mac-mini-de-Mario-5.local>
2026-02-12 09:44:39 -0600

    Throttle for ddos protection

--- doctorapp/settings.py
+++ doctorapp/settings.py
@@ -128,5 +128,13 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '1000/minute',
+    }
 }
